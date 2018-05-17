ab9f6735fc074ad7759e0cc47f83939890c2461d
Greg <wilusz.gregory@gmail.com>
2018-05-18 00:07:10 +0200

    Fixed bug with  object multiplication related with number of authors

--- app/src/main/java/com/example/android/newsfeedapp/QueryUtils.java
+++ app/src/main/java/com/example/android/newsfeedapp/QueryUtils.java
@@ -130,10 +130,9 @@ public final class QueryUtils {
 
                         String contributorName = contributor.getString("webTitle");
                         contributors.add(contributorName);
-
-                        News singleNewsObject = new News(section, title, publicationDate, contributors, webUrl);
-                        news.add(singleNewsObject);
                     }
+                    News singleNewsObject = new News(section, title, publicationDate, contributors, webUrl);
+                    news.add(singleNewsObject);
                 } else {
                     News singleNewsObject = new News(section, title, publicationDate, webUrl);
                     news.add(singleNewsObject);
